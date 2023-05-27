09a7cfb298d44e254d2b4892a5e2ddfb24975492
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-05-28 01:04:24 +0530

    💡 TOP10

---
+++ Difficulty/Easy/TOP10/solution.cpp
@@ -0,0 +1,20 @@
+// MASTERCHEF FINALS
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>10){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
