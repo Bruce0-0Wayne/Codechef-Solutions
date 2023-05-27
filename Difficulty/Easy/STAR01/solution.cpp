63df4a5e16bce65aa8985e311e2166b0b336e9e3
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-05-28 00:20:55 +0530

    STAR01

---
+++ Difficulty/Easy/STAR01/solution.cpp
@@ -0,0 +1,14 @@
+// Number Mirror
+#include <iostream>
+using namespace std;
+
+void nummirror(int n){
+    cout<<n;
+}
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	nummirror(x);
+	return 0;
+}
