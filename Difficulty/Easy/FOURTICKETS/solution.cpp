051523d898a756ed37dc92fa11460307a9eec370
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-05-31 23:23:10 +0530

    💡 FOURTICKETS

---
+++ Difficulty/Easy/FOURTICKETS/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(4*x<=1000){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
