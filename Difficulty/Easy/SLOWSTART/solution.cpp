daaa88cd5703d2b7b753c46a6588677bff956423
sahilansari189 <121338629+sahilansari189@users.noreply.github.com>
2023-10-02 15:36:01 +0530

    Update solution.cpp
    Add comments: Adding comments to your code can make it more understandable and maintainable. Explain the purpose of the code and what each part does.
    
    Use meaningful variable names: Variable names like x, h, count, and i are somewhat descriptive, but using more meaningful names can enhance code readability. For example, x could be renamed to stepSize, h to remainingHeight, count to totalSteps, and i to stepsTaken.
    
    Validate input: Check if the input values are valid or within acceptable ranges, and handle invalid input gracefully.

--- Difficulty/Easy/SLOWSTART/solution.cpp
+++ Difficulty/Easy/SLOWSTART/solution.cpp
@@ -2,23 +2,28 @@
 using namespace std;
 
 int main() {
-	// your code goes 
-	int t;
-	cin>>t;
-	while(t--){
-    	int x,h;
-    	cin>>x>>h;
-    	int  count = 0 ,i =0;
-	    while(i < 5 && h > 0){
-	        h = h - x/2;
-	        count ++;
-	        i++;
-	    }
-	    while(h > 0){
-	        h = h - x;
-	        count++;
-	    }
-	    cout<<count<<endl;
-	}
-	return 0;
+    int t;
+    cin >> t;
+    while (t--) {
+        int stepSize, remainingHeight;
+        cin >> stepSize >> remainingHeight;
+        int totalSteps = 0;
+        int stepsTaken = 0;
+
+        // First, use smaller steps (x/2) up to 5 steps or until height becomes non-positive
+        while (stepsTaken < 5 && remainingHeight > 0) {
+            remainingHeight -= stepSize / 2;
+            stepsTaken++;
+        }
+
+        // If there's still height to reduce, use larger steps (x)
+        while (remainingHeight > 0) {
+            remainingHeight -= stepSize;
+            stepsTaken++;
+        }
+
+        totalSteps = stepsTaken;
+        cout << totalSteps << endl;
+    }
+    return 0;
 }
