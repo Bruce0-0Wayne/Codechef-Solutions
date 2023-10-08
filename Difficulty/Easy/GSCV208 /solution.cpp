10eb6025a4d9ccf4923c0b58a9051bb555ac4342
Sharad Kumar <124381391+sharadnkp@users.noreply.github.com>
2023-10-08 23:06:37 +0530

    Create solution.cpp

---
+++ Difficulty/Easy/GSCV208 /solution.cpp
@@ -0,0 +1,14 @@
+// Update the '_' in the code below
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+ string A,B,C,D,E,F,G,H,I;
+ cin>>A>>B;
+ cin>>C>>D>>E;
+ cin>>F>>G>>H>>I;
+ cout<<A<<" "<<B<<" "<<C<<" "<<D<<" "<<E<<" "<<F<<" "<<G<<" "<<H<<" "<<I;
+  return 0;
+}
