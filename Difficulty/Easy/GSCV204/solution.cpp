35e3c012afca7688adbf31095895ee2ee648dd2b
Sharad Kumar <124381391+sharadnkp@users.noreply.github.com>
2023-10-08 23:04:25 +0530

    Create solution.cpp

---
+++ Difficulty/Easy/GSCV204/solution.cpp
@@ -0,0 +1,15 @@
+// Update the '_' in the code below
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+ int a = 5;
+ cout<<a<<endl;
+ 		
+ string b = "abcde";		
+ cout<<b<<endl;
+		
+ int array[] = {1,2,3}; 
+ cout<<array[0]<<" "<<array[1]<<" "<<array[2];	
+}
